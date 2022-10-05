5465088ff19792e1b28bf6d1705d8beb804fe72a
Marco González <marcoignacio.9637@gmail.com>
2022-10-05 11:41:05 -0300

    load all fyles

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Auth\AuthenticationException;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try {
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!$user || !auth()->guard()->attempt($request->only('email', 'password'))) {
+                throw new AuthenticationException();
+            }
+
+        } catch (\Exception $error) {
+            return $error->getMessage();
+        }
+    }
+}
