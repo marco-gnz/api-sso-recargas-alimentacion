28ecfabd9b85b13679996ec15ed61719a78c3824
Marco <marcoignacio.9637@gmail.com>
2023-06-28 11:33:02 -0400

    fix:correción de errores login history

--- app/Listeners/LogSuccessfulLogin.php
+++ app/Listeners/LogSuccessfulLogin.php
@@ -3,22 +3,25 @@
 namespace App\Listeners;
 
 use App\Models\LoginHistory;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
 
-class LogSuccessfulLogin implements ShouldQueue
+class LogSuccessfulLogin
 {
-    use InteractsWithQueue;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
 
     public function handle(Login $event)
     {
         try {
             $user = $event->user;
             LoginHistory::create([
-                'user_id' => $user->id,
-                'ip_address' => request()->ip(),
-                'login_at' => now(),
+                'user_id'       => $user->id,
+                'ip_address'    => $this->request->ip(),
+                'login_at'      => now(),
             ]);
         } catch (\Exception $e) {
             report($e);
